9ce7cd0abe05a754a8302c1330f355ca6f000d86
Timothy Lobanovskiy <93603887+Timloban@users.noreply.github.com>
2022-05-22 06:49:33 +0000

    Added css, and allowed fieldsets to work properly

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function guessingGame(){
   
   const guessingDom = document.getElementById("guessing");
   const playingDom  = document.getElementById("playing");
+  const finalDom    = document.getElementById("final");
   const guessDom    = document.getElementById("guess");
   const triesDom    = document.getElementById("tries");
   const hintDom     = document.getElementById("hint");
   const score       = document.getElementById("score");
   const ansDom      = document.getElementById("answer");
   const yesDom      = document.getElementById("yes");
-  const noDom       = document.getElementById("no");
   const guessesDom  = document.getElementById("guesses");
   const bestDom     = document.getElementById("best");
   const gamesDom    = document.getElementById("games");
+  const averageDom  = document.getElementById("avg");
 
   let answer = -1; 
   let tries  = 0;
@@ -28,6 +29,7 @@ function guessingGame(){
     guessDom.value = "";
     guessingDom.style.display = "unset";
     playingDom.style.display = "none";
+    finalDom.style.display = "";
 
     answer = Math.floor(Math.random() * 100) +1;
     ansDom.value = answer;
@@ -43,11 +45,12 @@ function guessingGame(){
 
   
   function makeGuess(e){
+    
     this.setAttribute("placeholder",this.value);
     
-    if      (this.value < answer) hintDom.innerHTML = "HIGHER! The answer is higher!";
-    else if (this.value > answer) hintDom.innerHTML = "LOWER! The answer is lower!";
-    else    showGameResults(tries);
+    if      (this.value < ansDom.value) hintDom.innerHTML = "HIGHER! The answer is higher!";
+    else if (this.value > ansDom.value) hintDom.innerHTML = "LOWER! The answer is lower!";
+    else if (this.value == ansDom.value) showGameResults(tries);
   
     triesDom.value = tries++;
     this.value = "";
@@ -56,18 +59,26 @@ function guessingGame(){
       guessDom.focus();
     },10);
   }
-
-
   
   function showGameResults(tries){
-    guessingDom.style.display = "none";
-    playingDom.style.display  = "unset";
+    
+    
+    //shows up in first box
     score.value = tries;
+    
     guessesDom.value = tries + parseInt(guessesDom.value);
     bestDom.value = getBest(tries);
     gamesDom.value = parseInt(gamesDom.value) + 1;
+    averageDom.value = (parseInt(guessesDom.value))/(parseInt(gamesDom.value));
+    playingDom.style.display  = "unset";
+    guessingDom.style.display = "none";
+    if (guessDom.value == ansDom.value) finalDom.style.display = "display";
+    
+    
     yesDom.focus();
   }
+
+    
 
 
   
@@ -76,7 +87,6 @@ function guessingGame(){
     if(check === 0 || count < check) return count;
     else return check;
   }
-  
 }
 
 
